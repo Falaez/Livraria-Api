05ed77a7f5b76da07dd5137067a760c91a5c0952
Mário Henrique <mariohenriquelp@gmail.com>
2021-11-26 15:48:36 -0300

    Arrumando teste

--- src/main/java/br/com/alura/livraria/dto/AutorDto.java
+++ src/main/java/br/com/alura/livraria/dto/AutorDto.java
@@ -1,5 +1,7 @@
 package br.com.alura.livraria.dto;
 
+import java.time.LocalDate;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,5 +11,8 @@ public class AutorDto {
 	
 	private Long id;
 	private String nome;
+	private String email;
+	private LocalDate dataNascimento;
+	private String miniCurriculo;
 	
 }
